82bac0f3691713a02c723cf4786c6eeab76bcfa9
Stilla <sunnatilla95@gmail.com>
2025-12-02 00:13:29 +0500

    login success

--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Nav from '../Components/Nav/Nav'
+import SideBar from '../Components/SideBar/SideBar'
+import { Outlet } from 'react-router'
 
 const Layout = () => {
     return (
         <div>
-            
+            <Nav />
+            <Outlet />
+            <SideBar />
         </div>
     )
 }
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
+    const { handleSubmit, register, reset } = useForm()
+    const [registerData, setRegisterData] = useState()
+
+    const onSubmit = (data) => {
+
+        reset()
+    }
+
+    return (
+        <div className='h-screen flex items-center'>
+            <div className='p-12 text-center shadow w-2/5 mx-auto rounded-lg'>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <input
+                        className='block mx-auto border border-gray-400 px-2 py-1 rounded-md mb-2'
+                        type="text"
+                        placeholder='Enter your login'
+                        {...register("login", { required: true })} />
+                    <input
+                        className='block mx-auto border border-gray-400 px-2 py-1 rounded-md mb-2'
+                        type="password"
+                        placeholder='Enter your password'
+                        {...register("password", { required: true })} />
+                    <button className='bg-sky-500 text-white p-2 rounded-md'>Login</button>
+                </form>
+            </div>
+        </div>
+    )
 }
 
 export default Login
